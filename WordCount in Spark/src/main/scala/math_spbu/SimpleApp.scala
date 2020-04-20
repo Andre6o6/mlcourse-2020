3f756c1808ef83275674a18cb8802b69d2c2d71b
Andrey Sugonyaev <sug.andre606@gmail.com>
2020-04-20 14:52:47 +0300

    save to txt and csv with spark functions

--- WordCount in Spark/src/main/scala/math_spbu/SimpleApp.scala
+++ WordCount in Spark/src/main/scala/math_spbu/SimpleApp.scala
@@ -1,21 +1,27 @@
 package math_spbu
 import org.apache.spark.SparkContext
-import java.io._
+import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
   def main(args: Array[String]): Unit = {
     var sc = new SparkContext("local[*]", "Example")
     var text = sc.textFile("src/main/data/ShadowOverInnsmouth.txt")
+
     val counts = text.flatMap(_.split(" "))
+      .map(_.replaceAll("[^\\w]","").toLowerCase)
+      .filter(!_.isEmpty)
       .map(word => (word, 1))
       .reduceByKey((a, b) => a + b)
       .sortBy(_._2, ascending = false)
-      .collect()
-    //counts.slice(0,10).foreach(println)
 
-    val pw = new PrintWriter(new File("out.txt" ))
-    for ((w, c) <- counts)
-      pw.println(w + " -  " + c)
-    pw.close
+    counts.coalesce(1).saveAsTextFile("results_text")
+
+    var sparkSession = SparkSession.builder().appName("Example").getOrCreate()
+    val df = sparkSession.createDataFrame(counts).toDF("word", "count")
+    df.coalesce(1)
+      .write
+      .option("header","true")
+      .mode("overwrite")
+      .csv("results_csv")
   }
 }
